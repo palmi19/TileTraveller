94e3a348003ff1916d6c3a83a76ed5f3ad2bd332
kumasi <Kumasimani19@gmail.com>
2019-09-17 10:23:08 +0000

    uppfært gridið

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-y = 3
-x = 3
-def grid(x,y):
-    return 1
+
+
+def grid(bordlength):
+    for row in range(1,bordlength + 1):
+        x = row
+
+        for cell in range(1,bordlength + 1):
+            y = cell
+
+    return x,y
+
+
+print(grid(3))
+
+
+def postion(x,y):
+
+
+
+
+starting_Position = (1,1)
+
+
+
+
+
+
+#def direction(input):
+
+
+
+ #   print("you can travel ( {} ) orth.".format())
+
+ #   return input
+
+
+
+#inputed = str(input("Direction: "))
